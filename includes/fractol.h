c2a1897427c4ac13e58620332c37db20b3c30b4b
big-shell <>
2021-12-05 15:36:29 +0100

    structures for window

--- includes/fractol.h
+++ includes/fractol.h
@@ -6,7 +6,7 @@
 /*   By: mjacq <mjacq@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/03 13:26:24 by mjacq             #+#    #+#             */
-/*   Updated: 2021/12/03 13:50:33 by mjacq            ###   ########.fr       */
+/*   Updated: 2021/12/05 15:31:10 by mjacq            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,43 @@
 
 # include "mlx.h"
 # include "libft.h"
+
+# define WIN_WIDTH 720
+# define WIN_HEIGHT 480
+
+typedef enum e_error
+{
+	success = 0,
+	error
+}	t_error;
+
+typedef struct s_win
+{
+	void		*win;
+	const char	*name;
+	int			width;
+	int			height;
+	t_error		error;
+}				t_win;
+
+typedef struct s_img
+{
+	char	*buf;
+	t_win	*win;
+}			t_img;
+
+typedef struct s_pixel
+{
+	int	x;
+	int	y;
+}		t_pixel;
+
+typedef struct s_root
+{
+	void	*mlx;
+	t_win	win;
+	t_error	error;
+}			t_root;
 
 int	main_fractol(int ac, const char *av[]);
 
--- src/main_fractol.c
+++ src/main_fractol.c
@@ -6,21 +6,44 @@
 /*   By: mjacq <mjacq@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/03 13:48:59 by mjacq             #+#    #+#             */
-/*   Updated: 2021/12/03 13:50:13 by mjacq            ###   ########.fr       */
+/*   Updated: 2021/12/05 15:35:20 by mjacq            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
 
+//TODO: error checking
+void	parse_args(t_root *root, int ac, const char *av[])
+{
+	if (ac == 2)
+		root->win.name = av[1];
+	else
+		root->win.name = "Fractol";
+}
+
+void	win_init(t_win *win, void **mlx)
+{
+	*mlx = mlx_init();
+	if (*mlx)
+	{
+		win->width = WIN_WIDTH;
+		win->height = WIN_HEIGHT;
+		win->win = mlx_new_window(*mlx, win->width, win->height, \
+				(char *)win->name);
+	}
+	if (!mlx || !win->win)
+		win->error = error;
+}
+
+	/* img_draw(circle); */
+	/* img_render(); */
 int	main_fractol(int ac, const char *av[])
 {
-	void	*mlx;
+	t_root	root;
 
-	(void)ac;
-	(void)av;
-	ft_putstr("Libft is linked.\n");
-	mlx = mlx_init();
-	mlx_new_window(mlx, 720, 480, "fractol");
-	mlx_loop(mlx);
+	root = (t_root){};
+	parse_args(&root, ac, av);
+	win_init(&root.win, &root.mlx);
+	mlx_loop(root.mlx);
 	return (0);
 }
