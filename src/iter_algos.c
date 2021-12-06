7a06172098aa55ca87c1f81717841bd485020759
mjacq <mjacq@student.42.fr>
2021-12-06 18:39:40 +0100

    main bud opti

--- src/iter_algos.c
+++ src/iter_algos.c
@@ -6,7 +6,7 @@
 /*   By: mjacq <mjacq@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/06 14:28:27 by mjacq             #+#    #+#             */
-/*   Updated: 2021/12/06 14:31:22 by mjacq            ###   ########.fr       */
+/*   Updated: 2021/12/06 18:39:35 by mjacq            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,10 +36,16 @@ int	iter_julia(t_coordinates *c, t_coordinates *zn, int max_iter)
 		return (1 + iter_julia(c, &znext, max_iter - 1));
 }
 
+/*
+** main bud opti : (x+1)**2 + y**2 < 1/16
+*/
+
 int	iter_mandelbrot(t_coordinates *c, int max_iter)
 {
 	t_coordinates	z0;
 
+	if (powf(c->x + 1, 2) + powf(c->y, 2) < 1.0 / 16)
+		return (max_iter);
 	z0 = (t_coordinates){};
 	return (iter_julia(c, &z0, max_iter));
 }
