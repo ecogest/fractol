4dc616817d922d57482d758697f22654f42aa6b1
ecogest <matthieu.jacq@live.fr>
2021-12-06 14:33:41 +0100

    first mandelbrot on display !! (#11)
    Co-authored-by: mjacq <mjacq@student.42.fr>

---
+++ src/iter_algos.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   iter_algos.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mjacq <mjacq@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/06 14:28:27 by mjacq             #+#    #+#             */
+/*   Updated: 2021/12/06 14:31:22 by mjacq            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fractol.h"
+
+/*
+** Given z0 (complex number), compute how many z_{n+1} = (z_n)**2 + c
+** are possible before the sequel diverges for sure (when |z| >= 2)
+*/
+
+int	iter_julia(t_coordinates *c, t_coordinates *zn, int max_iter)
+{
+	t_coordinates	zsquare;
+	t_coordinates	znext;
+	float			square_modulus_next;
+
+	if (!max_iter)
+		return (0);
+	zsquare.x = zn->x * zn->x - zn->y * zn->y;
+	zsquare.y = 2 * zn->x * zn->y;
+	znext.x = zsquare.x + c->x;
+	znext.y = zsquare.y + c->y;
+	square_modulus_next = znext.x * znext.x + znext.y * znext.y;
+	if (square_modulus_next >= 4)
+		return (0);
+	else
+		return (1 + iter_julia(c, &znext, max_iter - 1));
+}
+
+int	iter_mandelbrot(t_coordinates *c, int max_iter)
+{
+	t_coordinates	z0;
+
+	z0 = (t_coordinates){};
+	return (iter_julia(c, &z0, max_iter));
+}
