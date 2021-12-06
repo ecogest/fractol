40c46ed5d3a06115982939f56b1c464396f8bc3a
mjacq <mjacq@student.42.fr>
2021-12-06 09:55:38 +0100

    structure header

--- includes/fractol.h
+++ includes/fractol.h
@@ -6,12 +6,16 @@
 /*   By: mjacq <mjacq@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/03 13:26:24 by mjacq             #+#    #+#             */
-/*   Updated: 2021/12/05 16:19:03 by mjacq            ###   ########.fr       */
+/*   Updated: 2021/12/06 09:55:09 by mjacq            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FRACTOL_H
 # define FRACTOL_H
+
+/*
+** ================================ Includes ================================ **
+*/
 
 # include "mlx.h"
 # include "libft.h"
@@ -23,11 +27,32 @@
 # define WIN_WIDTH 720
 # define WIN_HEIGHT 480
 
+/*
+** ================================= Enums ================================== **
+*/
+
 typedef enum e_error
 {
 	success = 0,
 	error
 }	t_error;
+
+/*
+** =============================== Structures =============================== **
+*/
+
+typedef struct s_pixel
+{
+	int	x;
+	int	y;
+}		t_pixel;
+
+typedef struct s_circle
+{
+	float	r;
+	float	x0;
+	float	y0;
+}		t_circle;
 
 typedef struct s_win
 {
@@ -45,17 +70,15 @@ typedef struct s_img
 	t_win	*win;
 }			t_img;
 
-typedef struct s_pixel
-{
-	int	x;
-	int	y;
-}		t_pixel;
-
 typedef struct s_root
 {
 	t_win	win;
 	t_error	error;
 }			t_root;
+
+/*
+** =============================== Functions ================================ **
+*/
 
 int		main_fractol(int ac, const char *av[]);
 void	win_init(t_win *win);
